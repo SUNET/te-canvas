157cea7ff3c810f0829fcfdb271ee59567ef6071
Kristofer Hallin <kristofer@sunet.se>
2021-08-27 12:55:06 +0200

    Version API endpoint added as example.

---
+++ te_canvas/api/version.py
@@ -0,0 +1,20 @@
+from flask import request
+from flask_restx import Resource, Namespace
+
+
+version_api = Namespace('version', description='Version API',
+                        prefix='/api')
+
+
+class VersionApi(Resource):
+    def get(self):
+        return {
+            'status': 'success',
+            'data': {
+                'version':
+                'v0.0.1'
+            }
+        }
+
+
+version_api.add_resource(VersionApi, '')
--- te_canvas/app.py
+++ te_canvas/app.py
@@ -3,7 +3,7 @@ import os
 from flask_cors import CORS
 from flask import Flask
 from flask_restx import Api
-
+from te_canvas.api.version import version_api
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(128)
@@ -12,3 +12,4 @@ cors = CORS(app,
             resources={r"/api/*": {"origins": "*"}})
 
 api = Api(app, prefix='/api')
+api.add_namespace(version_api)
