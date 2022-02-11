5bf8bfa9de71fb105c92c299a2f661f6ed5f9c61
Ernst Widerberg <ernst@sunet.se>
2022-02-11 17:21:36 +0100

    Improve unit test

--- te_canvas/test/test_canvas.py
+++ te_canvas/test/test_canvas.py
@@ -1,16 +1,33 @@
+import os
+import sys
 import unittest
-import te_canvas.canvas as canvas
 
+import canvasapi
+import canvasapi.exceptions
+
+import te_canvas.canvas
+
+try:
+    url = os.environ["CANVAS_URL"]
+    key = os.environ["CANVAS_KEY"]
+except Exception as e:
+    print(f"Failed to load configuration: {e}")
+    sys.exit(-1)
+
+canvas = canvasapi.Canvas(url, key)
 
 class TestCanvas(unittest.TestCase):
-
     def test_delete_event(self):
+        id = 0
+
+        with self.assertRaises(canvasapi.exceptions.ResourceDoesNotExist):
+            canvas.get_calendar_event(id)
+
         try:
-            canvas.delete_event(0)
+            te_canvas.canvas.delete_event(id)
         except Exception:
             self.fail()
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
