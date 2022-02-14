3a4ef3c3eab96ffa21839e323c7fe895448af48b
Ernst Widerberg <ernst@sunet.se>
2022-02-14 12:02:28 +0100

    Finish unit tests for te_canvas.canvas

--- te_canvas/test/test_canvas.py
+++ te_canvas/test/test_canvas.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import time
 import unittest
 
 import canvasapi
@@ -16,17 +17,56 @@ except Exception as e:
 
 canvas = canvasapi.Canvas(url, key)
 
+# NOTE: Statements are implicitly assumed to succeed, since they all should
+# throw exceptions which (if they are not caught) register in the test results.
+
+
 class TestCanvas(unittest.TestCase):
-    def test_delete_event(self):
+    def test_get_courses_all(self):
+        """Getting all courses should succeed."""
+        courses = te_canvas.canvas.get_courses_all()
+        self.assertGreater(
+            len(courses), 0, "get_courses_all should return at least one course."
+        )
+
+    def test_create_event_success(self):
+        """Creating an event should succeed."""
+        canvas_event = te_canvas.canvas.create_event(
+            {
+                "context_code": "course_168",  # Course "ernst_test"
+                "title": "unittest_title",
+                "location_name": "unittest_location",
+                "description": "unittest_description",
+                "start_at": "20220314T120000",
+                "end_at": "20220314T130000",
+            }
+        )
+        te_canvas.canvas.delete_event(canvas_event.id)
+
+    def test_create_event_fail(self):
+        """Creating an event should fail if the course does not exist."""
+        with self.assertRaises(canvasapi.exceptions.ResourceDoesNotExist):
+            te_canvas.canvas.create_event(
+                {
+                    "context_code": "course_9999",
+                    "title": "unittest_title",
+                    "location_name": "unittest_location",
+                    "description": "unittest_description",
+                    "start_at": "20220314T120000",
+                    "end_at": "20220314T130000",
+                }
+            )
+
+    def test_delete_event_non_existing(self):
+        """Deleting a non-existing event should not cause an exception."""
         id = 0
 
+        # Make sure that the event does not exist
         with self.assertRaises(canvasapi.exceptions.ResourceDoesNotExist):
             canvas.get_calendar_event(id)
 
-        try:
-            te_canvas.canvas.delete_event(id)
-        except Exception:
-            self.fail()
+        # Try to delete it anyway
+        te_canvas.canvas.delete_event(id)
 
 
 if __name__ == "__main__":
