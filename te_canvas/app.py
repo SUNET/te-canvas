59ba6d824cb93e24f49f3c479127a258dd5f3899
Ernst Widerberg <ernst@sunet.se>
2022-02-16 16:31:50 +0100

    Bugfix

--- te_canvas/app.py
+++ te_canvas/app.py
@@ -2,10 +2,10 @@ import os
 
 from flask import Flask, request
 from flask_cors import CORS
-from flask_restx import Api
+from flask_restx import Api, Namespace
 
 from te_canvas.api.canvas import canvas_api
-from te_canvas.api.connection import connection_api, ConnectionApi
+from te_canvas.api.connection import ConnectionApi
 from te_canvas.api.timeedit import timeedit_api
 from te_canvas.api.version import version_api
 from te_canvas.log import get_logger
@@ -28,7 +28,12 @@ class App:
         api.add_namespace(timeedit_api)
         api.add_namespace(canvas_api)
 
-        connection_api.add_resource(ConnectionApi, "", resource_class_kwargs={"db": db})
+        connection_api = Namespace(
+            "connection",
+            description="API for handling connections between TimeEdit and Canvas",
+            prefix="/api",
+        )
+        connection_api.add_resource(ConnectionApi, "", resource_class_kwargs={"db": self.db})
         api.add_namespace(connection_api)
 
         @self.flask.after_request
