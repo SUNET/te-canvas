bccce017e285e455e897d0c17d5e1338fc0b36f2
Ernst Widerberg <ernst@sunet.se>
2022-04-04 14:29:58 +0200

    Fix: Use BlockingScheduler

--- te_canvas/job.py
+++ te_canvas/job.py
@@ -1,4 +1,4 @@
-from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.background import BlockingScheduler
 from pytz import utc
 
 from te_canvas.log import get_logger
@@ -9,22 +9,22 @@ logger = get_logger()
 
 class JobScheduler(object):
     def __init__(self):
-        self._scheduler = BackgroundScheduler(timezone=utc)
+        self.__scheduler = BlockingScheduler(timezone=utc)
 
     def get(self):
-        return self._scheduler
+        return self.__scheduler
 
     def start(self):
         logger.info("Starting scheduler.")
-        return self._scheduler.start()
+        return self.__scheduler.start()
 
     def stop(self):
         logger.info("Stopping scheduler.")
-        return self._scheduler.shutdown()
+        return self.__scheduler.shutdown()
 
     def add(self, func, seconds, kwargs):
         logger.info(f"Adding job to scheduler: interval={seconds}")
-        return self._scheduler.add_job(func, "interval", seconds=seconds, kwargs=kwargs)
+        return self.__scheduler.add_job(func, "interval", seconds=seconds, kwargs=kwargs)
 
 
 if __name__ == "__main__":
