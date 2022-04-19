1f043b5af3d91399b4bc27760a405ad15b2f561e
Ernst Widerberg <ernst@sunet.se>
2022-04-19 13:03:29 +0200

    Add instruction

--- symlink.sh
+++ symlink.sh
@@ -2,6 +2,7 @@
 
 # 1. Create a dir `customer` in the same dir as `te-canvas` and `te-canvas-front`
 # 2. cd into `customer` and execute `../te-canvas/symlink.sh`
+# 3. docker-compose -f front.yml -f back.yml --profile api --profile sync up -d
 
 ln -s ../te-canvas/docker-compose.yml back.yml
 ln -s ../te-canvas-front/docker-compose.yml front.yml
