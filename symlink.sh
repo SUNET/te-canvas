af384930b3cbe02ee1de3290af1206b561f7d978
Ernst Widerberg <ernst@sunet.se>
2022-04-29 09:01:19 +0200

    Update symlink.sh

--- symlink.sh
+++ symlink.sh
@@ -2,10 +2,9 @@
 
 # 1. Create a dir `customer` in the same dir as `te-canvas` and `te-canvas-front`
 # 2. cd into `customer` and execute `../te-canvas/symlink.sh`
-# 3. docker-compose -f front.yml -f back.yml --profile api --profile sync up -d
+# 3. Add symlinks ssl.crt, ssl.key pointing to cert keypair
+# 4. docker-compose -f front.yml -f back.yml --profile api --profile sync up -d
 
 ln -s ../te-canvas/docker-compose.yml back.yml
 ln -s ../te-canvas-front/docker-compose.yml front.yml
-ln -s ../te-canvas-front/ssl.crt .
-ln -s ../te-canvas-front/ssl.key .
 ln -s ../te-canvas-front/nginx-docker.conf .
