ce6c143a69f1588a924699b128cee2c277a5c89a
Ernst Widerberg <ernst@sunet.se>
2022-06-09 16:41:29 +0200

    Documentation: Translator

--- te_canvas/translator.py
+++ te_canvas/translator.py
@@ -1,12 +1,14 @@
+"""
+This module gathers functionality for "translating" a TimeEdit event into a Canvas event and dealing
+with templates related to this.
+"""
+
 import re
 from string import Template
 
 from sqlalchemy.exc import NoResultFound  # type: ignore
 
 from te_canvas.util import State
-
-# This module gathers functionality for "translating" a TimeEdit event into a Canvas event and
-# dealing with templates related to this.
 
 # Used to identify synced events
 TAG_TITLE = r"​"
@@ -16,10 +18,14 @@ class TemplateError(Exception):
     pass
 
 
-# This class reads from the database only at initialization. So for any given Translator instance t,
-# t.canvas_event is a pure function.
 class Translator:
-    # Raises TemplateError
+    """
+    Translator creates Canvas events from TimeEdit events based on a set of template strings.
+
+    Template strings are read from the database only at Translator initialization. So for any given
+    Translator instance t, t.canvas_event is a pure function.
+    """
+
     def __init__(
         self,
         db,
@@ -28,6 +34,14 @@ class Translator:
         location=None,
         description=None,
     ):
+        """
+        Read template strings from the database and extract fields (used for template functionality)
+        and return types (used as arguments in TimeEdit API calls).
+
+        Raises:
+            TemplateError, if any of the required template strings (title, location, description) is
+            missing or empty.
+        """
         self.db = db
         self.timeedit = timeedit
         self.template_title = title or self.template("title")
@@ -52,16 +66,22 @@ class Translator:
             "end_at":   timeedit_reservation["end_at"],
             }  # fmt: skip
 
-    # Used for change detection in sync job
     def state(self) -> State:
+        """
+        Return an object allowing instances of Translator to be compared.
+
+        Used for change detection in sync.py.
+        """
         return {
             "title": self.template_title,
             "location": self.template_location,
             "description": self.template_description,
         }
 
-    # Check if we have a complete event template definition
     def template_config_ok(self) -> bool:
+        """
+        Check if we have a complete event template definition.
+        """
         try:
             for k in ["title", "location", "description"]:
                 self.template(k)
@@ -69,8 +89,10 @@ class Translator:
             return False
         return True
 
-    # Get template string, raise TemplateError if missing or empty
     def template(self, key: str) -> str:
+        """
+        Get a template string, raise TemplateError if missing or empty.
+        """
         try:
             res = self.db.get_config(key)
         except NoResultFound:
@@ -84,12 +106,23 @@ class Translator:
 
 SEPARATOR = "::"
 
-# Subclass to allow any characters in braced identifiers. We consider only braced identifiers.
+
 class MyTemplate(Template):
+    """
+    We use a custom subclass of Template to allow any characters in braced identifiers.
+
+    We consider only braced identifiers.
+    """
+
     braceidpattern = r"[^}]*"
 
 
 def _string(template, fields, objects):
+    """
+    Produce a string with variables substituted.
+
+    Variables of the form ${foo::bar} are substituted for the field bar on object foo.
+    """
     s = MyTemplate(template)
     return s.substitute(_encode_fields(fields, objects))
 
@@ -101,12 +134,18 @@ def _encode_fields(tuples, objects):
     }
 
 
-# Dict { T: F[] }, meaning that for objects of type T we want to get fields F
+def _extract_fields(template: str) -> list[tuple[str, str]]:
+    """
+    Extract (type, field)-tuples from a template string.
+    """
+    return [tuple(pat.split(SEPARATOR)) for pat in re.findall(r"(?<=\${).*?(?=})", template)]
+
+
 def _return_types(tuples):
+    """
+    Extract return types from a list of (type, field)-tuples.
+    Returns:
+        A dict { T: F[] }, meaning that for objects of type T we want to get fields F.
+    """
     types = set([type for (type, _) in tuples])
     return {type: [field for (type_, field) in tuples if type_ == type] for type in types}
-
-
-# Extract (type, field) tuples from a template string
-def _extract_fields(template: str) -> list[tuple[str, str]]:
-    return [tuple(pat.split(SEPARATOR)) for pat in re.findall(r"(?<=\${).*?(?=})", template)]
