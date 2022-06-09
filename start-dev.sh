118d3159dbd76e04db9d1f5673a70185360ac9ff
Ernst Widerberg <ernst@sunet.se>
2022-06-09 14:18:05 +0200

    Start scripts: Add -d

--- start-dev.sh
+++ start-dev.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml --profile sync --profile api up
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml --profile sync --profile api up -d
--- start-prod.sh
+++ start-prod.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose --profile sync --profile api up
+docker-compose --profile sync --profile api up -d
