82a18f595dfee4041cb2c3d53b00aeb8bcc5e9e7
Ernst Widerberg <ernst@sunet.se>
2022-06-13 14:31:45 +0200

    Remove unused import

--- te_canvas/sync.py
+++ te_canvas/sync.py
@@ -12,7 +12,7 @@ from te_canvas.canvas import Canvas
 from te_canvas.db import DB, Connection, Event, flat_list
 from te_canvas.log import get_logger
 from te_canvas.timeedit import TimeEdit
-from te_canvas.translator import TAG_TITLE, TemplateError, Translator
+from te_canvas.translator import TemplateError, Translator
 from te_canvas.util import State
 
 
